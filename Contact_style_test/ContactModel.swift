7c0c8f0cce062308013ed2c86d6812180091302e
Gregory Keeley <GregoryKeeley@pursuit.org>
2020-05-23 18:05:01 -0400

    - Updated contact struct to class - Able to get information from the contact

--- Contact_style_test/ContactModel.swift
+++ Contact_style_test/ContactModel.swift
@@ -6,18 +6,59 @@
 //  Copyright © 2020 Gregory Keeley. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Contacts
 
-struct CustomContact {
-    let name: String
-    let email: String
-    let dob: Date
-    
-    static func getTestData() -> [CustomContact] {
-        let contact1 = CustomContact.init(name: "Gregory", email: "greg@greg.com", dob: Date())
-        let contact2 = CustomContact.init(name: "Meghan", email: "Whatever@gmail.com", dob: Date())
-        let contact3 = CustomContact.init(name: "Earl", email: "bird@gmail.com", dob: Date())
-        let testData = [contact1, contact2, contact3]
-        return testData
+
+class CustomContact {
+    let firstName: String
+    let lastName: String
+    let workEmail: String
+    var identifier: String?
+    var storedContact: CNMutableContact?
+    var phoneNumberField: (CNLabeledValue<CNPhoneNumber>)?
+    //Note: If a property is constant, so created with let, you have to initialize it in place or in the init method, even if it is an Optional.
+    init(firstName: String, lastName: String, workEmail: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.workEmail = workEmail
+    }
+    static func defaultContacts() -> [CustomContact] {
+        return [
+            CustomContact(firstName: "Greg", lastName: "Keeley", workEmail: "Greg@greg.com"),
+            CustomContact(firstName: "Meghan", lastName: "Calderone", workEmail: "Whatever@gmail.com"),
+            CustomContact(firstName: "Earl", lastName: "Pearl", workEmail: "BeautifulBirdie@yahoo.com")
+        ]
+    }
+}
+
+extension CustomContact: Equatable {
+    static func ==(lhs: CustomContact, rhs: CustomContact) -> Bool{
+        return lhs.firstName == rhs.firstName &&
+            lhs.lastName == rhs.lastName &&
+            lhs.workEmail == rhs.workEmail
+    }
+}
+extension CustomContact {
+    var contactValue: CNContact {
+        let contact = CNMutableContact()
+        contact.givenName = firstName
+        contact.familyName = lastName
+        contact.emailAddresses = [CNLabeledValue(label: CNLabelHome, value: workEmail as NSString)]
+        if let phoneNumberField = phoneNumberField {
+            contact.phoneNumbers.append(phoneNumberField)
+        }
+        return contact.copy() as! CNContact
+    }
+    //convenience initializer: A convenience initializer is a secondary initializer that must call a designated initializer of the same class. It is useful when you want to provide default values or other custom setup. A class does not require convenience initializers.
+    convenience init?(contact: CNContact) {
+        guard let email = contact.emailAddresses.first else { return nil }
+        let firstName = contact.givenName
+        let lastName = contact.familyName
+        let workEmail = email.value as String
+        self.init(firstName: firstName, lastName: lastName, workEmail: workEmail)
+        if let contactPhone = contact.phoneNumbers.first {
+            phoneNumberField = contactPhone
+        }
     }
 }
--- Contact_style_test/allContactsViewController.swift
+++ Contact_style_test/allContactsViewController.swift
@@ -14,27 +14,47 @@ class allContactsViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var testContacts = CustomContact.getTestData()
+    var testContacts = CustomContact.defaultContacts()
+    
     var contacts = [CNContact]()
     
+//MARK:- ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
     }
+    
     private func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
     }
-    //MARK:- Presents the native iOS contacts View Controller to ge the user
+    
+//MARK:- Presents the native iOS contacts View Controller to ge the user
     @IBAction func addContactButtonPressed(_ sender: UIBarButtonItem) {
         let contactPicker = CNContactPickerViewController()
         contactPicker.delegate = self
+        //Note: This is where we can create a predicate to only allow certain contacts on the CNContactPickerViewController to be highlighted
+        //Question: How to create a predicate? The formatting below is not something I am familiar with. It's contained within a string and has an "@" before the condition
+        contactPicker.predicateForEnablingContact = NSPredicate(format: "emailAddresses.@count > 0")
         present(contactPicker, animated: true)
     }
 }
+//MARK:- CNContactPickerDelegate
 extension allContactsViewController: CNContactPickerDelegate {
-    
+    //Note: There is a delegate for a single contact and multiple!
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contacts: [CNContact]) {
+        //Note: This compactMap uses the contact from the convenience init in CustomContact class
+        let contacts = contacts.compactMap { CustomContact(contact: $0) }
+        for contact in contacts {
+            if !testContacts.contains(contact) {
+                testContacts.append(contact)
+            }
+        }
+        tableView.reloadData()
+    }
 }
+
+//MARK:- TableViewDelegate & DataSource
 extension allContactsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let editViewController = TableViewController()
@@ -52,8 +72,8 @@ extension allContactsViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
         let contact = testContacts[indexPath.row]
-        cell.textLabel?.text = contact.name
-        cell.detailTextLabel?.text = contact.email
+        cell.textLabel?.text = contact.firstName
+        cell.detailTextLabel?.text = contact.workEmail
         return cell
     }
     
